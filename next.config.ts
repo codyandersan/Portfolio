f0bdcfc7d8f36164a3bf021e1a3976aa774123cd
Prakhar Aditya Tripathi <prakhar.ad.tr@gmail.com>
2026-05-10 20:36:02 +0530

    Add output configuration to next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
+  output: 'export',
   allowedDevOrigins: ['127.0.0.1', '*.app.github.dev', '*.github.dev', '172.28.151.122'],
 };
 
